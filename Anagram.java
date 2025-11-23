80e90ef141be9a1ea55de994c2965838978f1bac
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2025-11-24 01:25:35 +0200

    Update Algebra.java

--- Anagram.java
+++ Anagram.java
@@ -1,24 +1,42 @@
-/** Functions for checking if a given string is an anagram. */
+// Anagram.java
 public class Anagram {
-	public static String normalize(String a) {
-    if (a == null || a.isEmpty()) return "";
-    StringBuilder cleaned = new StringBuilder(a.length());
-    for (int i = 0; i < a.length(); i++) {
-        char ch = a.charAt(i);
-        if (Character.isLetter(ch)) cleaned.append(Character.toLowerCase(ch));
-    }
-    return cleaned.toString();
 
-	}
-	public static boolean areAnagrams(String a, String b) {
-    String Anorm = normalize(a), Bnorm = normalize(b);
-    if (Anorm.length() != Bnorm.length()) return false;
-    int[] counts = new int[26];
-    for (int i = 0; i < Anorm.length(); i++) {
-        counts[Anorm.charAt(i) - 'a']++;
-        counts[Bnorm.charAt(i) - 'a']--;
+    public static String preProcess(String s) {
+        if (s == null || s.isEmpty()) return "";
+        StringBuilder out = new StringBuilder(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {
+                out.append(Character.toLowerCase(c));
+            }
+        }
+        return out.toString();
     }
-    for (int c : counts) if (c != 0) return false;
-    return true;
-	}	
-}		
+
+    public static boolean isAnagram(String a, String b) {
+        String s1 = preProcess(a);
+        String s2 = preProcess(b);
+        if (s1.length() != s2.length()) return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+        for (int x : freq) {
+            if (x != 0) return false;
+        }
+        return true;
+    }
+
+    public static String randomAnagram(String s) {
+        String t = preProcess(s);
+        char[] a = t.toCharArray();
+        java.util.concurrent.ThreadLocalRandom r = java.util.concurrent.ThreadLocalRandom.current();
+        for (int i = a.length - 1; i > 0; i--) {
+            int j = r.nextInt(i + 1);
+            char tmp = a[i]; a[i] = a[j]; a[j] = tmp;
+        }
+        return new String(a);
+    }
+}
